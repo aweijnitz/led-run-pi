3d037dae7bfd9681237c6ca73a73ae4bb6d7e869
anders <andersw@noone.it>
2018-11-06 22:31:31 +0100

    Forming game loop

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const makeDir = require('make-dir');
 // SETUP
 //
 const conf = {
-    logDir: './logs'
+    logDir: './logs',
+    gameLoopDelayMs: 40 // 25 FPS
 };
 
 makeDir.sync(conf.logDir);
@@ -17,8 +18,8 @@ const logger = winston.createLogger({
         // - Write to all logs with level `info` and below to `combined.log`
         // - Write all logs error (and below) to `error.log`.
         //
-        new winston.transports.File({ filename: conf.logDir + '/error.log', level: 'error' }),
-        new winston.transports.File({ filename: conf.logDir + '/combined.log' })
+        new winston.transports.File({filename: conf.logDir + '/error.log', level: 'error'}),
+        new winston.transports.File({filename: conf.logDir + '/combined.log'})
     ]
 });
 
@@ -34,6 +35,74 @@ if (process.env.NODE_ENV !== 'production') {
 
 logger.info('App starting');
 
+// Load levels
+//
+logger.info('Loading levels');
 
 
+// Init display
+//
+logger.info('Init display');
 
+
+// Wait for game to start (run demo mode, poll sensor)
+//
+const GAME_RUNNING = 0;
+const GAME_STARTING = 1;
+const GAME_OVER = 2;
+const GAME_DEMO_LOOP = 3;
+
+let forever = 0;
+let mode = GAME_STARTING;
+let gameLoopId = 0;
+
+
+process.on('SIGTERM', function () {
+    clearInterval(forever);
+});
+
+const gameLoop = (dt, logr) => {
+    logr.info('GAME LOOP ' + dt());
+};
+
+let oldT = Date.now();
+let gameStarted = oldT;
+setInterval(() => {
+
+    switch (mode) {
+        case GAME_OVER:
+            logger.info('Game over');
+            clearInterval(gameLoopId);
+            gameLoopId = 0;
+            mode = GAME_DEMO_LOOP;
+            break;
+        case GAME_DEMO_LOOP:
+            break;
+        case GAME_STARTING:
+            // Player started game
+            // - Init world, display game begin transition, then clear screen
+            // Init World
+            //
+
+            // Game Loop!
+            //
+            logger.info('Starting game');
+            gameLoopId = setInterval(gameLoop, conf.gameLoopDelayMs,
+                () => {
+                    let now = Date.now();
+                    let dt = now - oldT;
+                    oldT = now;
+                    return dt;
+                }, logger);
+            mode = GAME_RUNNING;
+            break;
+        case GAME_RUNNING:
+            if(Date.now() - gameStarted > 3000)
+                mode = GAME_OVER;
+            break;
+        default:
+            break;
+    }
+
+
+}, 3);

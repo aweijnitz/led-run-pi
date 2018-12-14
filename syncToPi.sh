efc4fc801697e111adab5d0732e160a87561e657
anders <andersw@noone.it>
2018-12-14 10:20:26 +0100

    Sync without password

--- syncToPi.sh
+++ syncToPi.sh
@@ -1,1 +1,1 @@
-rsync -avz --exclude={node_modules,.idea,.git,.nyc_output,syncToPi.sh} ./ pi@192.168.178.37:/home/pi/projects/led-run-pi/
+rsync -avz -e "ssh -i ~/.ssh/pi" --exclude={node_modules,.idea,.git,.nyc_output,syncToPi.sh} ./ pi@192.168.178.37:/home/pi/projects/led-run-pi/

5d3cda0924a10c3de52456fc8481d1003ae172f3
anders <andersw@noone.it>
2018-12-15 00:51:47 +0100

    Buggy demo loop, but game can be started by holding hand < 3 cm for 5s

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const demoLoop = require('./lib/demoLoop');
 
 const levels = require('./config/levels');
 const World = require('./lib/World').World;
+const Controller = require('./lib/UltrasonicController');
+let controller = undefined;
+
+
 
 const GAME_RUNNING = 0;
 const GAME_STARTING = 1;
 const GAME_OVER = 2;
-const GAME_DEMO_LOOP = 3;
+const GAME_DEMO_LOOP_STARTING = 3;
+const GAME_DEMO_LOOP = 4;
 
 // SETUP
 //
 const conf = {
     logDir: './logs',
-    gameLoopDelayMs: 1000/40, // 40 Hz
-    renderLoopDelayMs: 1000/50 // 60 FPS
+    gameLoopDelayMs: 1000 / 40, // 40 Hz
+    renderLoopDelayMs: 1000 / 50 // 50 FPS
 };
 const logger = initLogger(conf);
 
@@ -26,21 +31,27 @@ const initWorld = function initWorld(levels, logger, gameSpeed) {
     return world;
 };
 
+
+
 logger.info('App starting');
 
+if(!controller) {
+    controller = new Controller(100, logger);
+    controller.start();
+}
 
 // Init display
 //
 logger.info('Init display');
 
-// TODO: Clear LED strip and set to black.
+//clearScreen();
 
 // Wait for game to start (run demo mode, poll sensor)
 //
 
 
 let forever = 0;
-let state = GAME_STARTING; // TODO: Should eventually start in GAME_DEMO_LOOP
+let state = GAME_DEMO_LOOP_STARTING;
 let gameLoopId = 0;
 let demoLoopId = 0;
 let renderLoopId = 0;
@@ -49,20 +60,26 @@ let renderLoopId = 0;
 process.on('SIGTERM', function () {
     clearInterval(gameLoopId);
     clearInterval(forever);
+    //clearScreen();
     console.log('SHUTDOWN (TERMINATED)');
     process.exit(0);
 });
 process.on('SIGINT', function () {
     clearInterval(gameLoopId);
     clearInterval(forever);
+    //clearScreen();
     console.log('SHUTDOWN (INTERRUPTED)');
     process.exit(0);
 });
 
 let oldT = Date.now();
+let startT = Date.now();
 let gameStarted = oldT;
 let gameSpeed = 1.0;
 let currentLevel = 0;
+let startGameTriggerTimout = 5*1000;
+let isTrackingGameTrigger = false;
+
 forever = setInterval(() => {
 
     switch (state) {
@@ -73,19 +90,38 @@ forever = setInterval(() => {
             clearInterval(renderLoopId);
 
             // TODO: Display game over transition, then switch state to demo loop
+            //clearScreen();
+            state = GAME_DEMO_LOOP_STARTING;
+
+            break;
+        case GAME_DEMO_LOOP_STARTING:
             logger.info('Starting demo loop');
             oldT = Date.now();
-            demoLoopId = setInterval(gameLoop, conf.gameLoopDelayMs,
+            startT = Date.now();
+            demoLoopId = setInterval(demoLoop, conf.gameLoopDelayMs,
                 () => {
                     let now = Date.now();
                     let dt = now - oldT;
                     oldT = now;
-                    return dt;
-                }, world, logger);
-            renderLoopId = setInterval(renderLoop, conf.renderLoopDelayMs, world, logger);
+                    return { dt: dt, startT: startT };
+                }, logger);
+            //renderLoopId = setInterval(renderLoop, conf.renderLoopDelayMs, world, logger);
             state = GAME_DEMO_LOOP;
             break;
         case GAME_DEMO_LOOP:
+            // CHeck for game start
+            let dist = controller.getCurrentDistance();
+            if(dist < 3 && !isTrackingGameTrigger) {
+                isTrackingGameTrigger = true;
+                startT = Date.now();
+            } else if (dist > 3 && isTrackingGameTrigger && Date.now() < (startT + startGameTriggerTimout)) {
+                isTrackingGameTrigger = false;
+            } else if (dist < 3 && isTrackingGameTrigger && Date.now() >= (startT + startGameTriggerTimout)) {
+                isTrackingGameTrigger = false;
+                clearInterval(demoLoopId);
+                state = GAME_STARTING;
+                logger.info('Player ready! Preparing to start game.');
+            }
             break;
         case GAME_STARTING:
             // Player started game
@@ -94,6 +130,7 @@ forever = setInterval(() => {
             // Start Game Loop!
             //
             logger.info('Starting game. LEVEL: ' + currentLevel);
+            //clearScreen();
             world = initWorld(levels, logger, gameSpeed);
             world.initLevel(currentLevel);
             oldT = Date.now();
@@ -103,7 +140,7 @@ forever = setInterval(() => {
                     let dt = now - oldT;
                     oldT = now;
                     return dt;
-                }, world, logger);
+                }, world, controller, logger);
             clearInterval(renderLoopId); // Kill demo loop
             renderLoopId = setInterval(renderLoop, conf.renderLoopDelayMs, world, logger);
             state = GAME_RUNNING;
